9d0cbe205ce6bb4b24efca2af2c0f86b2ef74a07
Suz Hinton <suz.hinton@gmail.com>
2017-03-04 20:07:34 -0500

    add basic install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-pip
+sudo pip install -U platformio
+npm install
